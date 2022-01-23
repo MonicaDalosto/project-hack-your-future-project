aa2ef2154e7d73320201bd74f9f69a008bd089cb
MonicaDalosto <monica.kerber@gmail.com>
2022-01-23 16:09:24 +0100

    Update Amigurumi Project
    - Style the navbar menu;
    - Create of JS function to set the class active, which style the mobile menu;

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the .iconMenu */
 function showNavBar() {
-  let navMenuElement = document.getElementById("myNavMenu");
-  if (navMenuElement.className === "navMenu") {
-    navMenuElement.className += "responsiveMenu";
+  let menuMobile = document.getElementById("menuMobile");
+  if (menuMobile.className.includes('active')) {
+    menuMobile.classList.remove('active');
   } else {
-    navMenuElement.className = "navMenu";
+    menuMobile.classList.add('active');
   }
 }
