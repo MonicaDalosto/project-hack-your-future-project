d6e044fdb6bfb4f79240c78b2713fbe6ed79e445
MonicaDalosto <monica.kerber@gmail.com>
2022-01-24 21:08:56 +0100

    Update Amigurumi project
    - I inspected the Javascript code;
    - I added the cursor:pointer to the .carouselTools;

--- carousel.js
+++ carousel.js
@@ -2,19 +2,13 @@
 
 let paused = false; // whether the slideshow is paused or not
 let counter = 1; // the counter variable that keeps track of which image you are showing
-let forwardButton = document.getElementById('forward');
-let backwardButton = document.getElementById('backward');
-let pauseButton = document.getElementById('pause');
 
-const changeElementPattern = (buttonElement) => {
+const changeElementPattern = (clickedButtonFunction) => {
   const elementPattern = document.getElementById("pattern" + counter);
   if(!elementPattern.classList.contains('carouselInactive')) {
     elementPattern.classList.add('carouselInactive');
-    console.log(elementPattern);
-    buttonElement()
-    console.log(counter);
+    clickedButtonFunction()
     const nextElementPattern = document.getElementById("pattern" + counter);
-    console.log(nextElementPattern);
     nextElementPattern.classList.remove('carouselInactive');
   }
 }
@@ -37,111 +31,21 @@ function changeValuePaused() {
   paused = !paused;
 }
 
+let forwardButton = document.getElementById('forward');
 forwardButton.addEventListener('click', function() {
   changeElementPattern(forward);
 });
+
+let backwardButton = document.getElementById('backward');
 backwardButton.addEventListener('click', function() {
   changeElementPattern(backward);
 });
+
+let pauseButton = document.getElementById('pause');
 pauseButton.addEventListener('click', changeValuePaused);
 
 setInterval(() => {
   if (!paused) {
-    console.log('deu certo');
     changeElementPattern(forward);
   }
 }, 3000);
-
-
-
-
-// -- IMPORTANT: -- Second Functions, they are working:
-// const forward = () => {
-//   const elementPattern = document.getElementById("pattern" + counter);
-//   if(!elementPattern.classList.contains('carouselInactive')) {
-//     elementPattern.classList.add('carouselInactive');
-//     counter = counter + 1;
-//     if(counter > 3){
-//       counter = 1;
-//     }
-//     console.log(counter);
-//     const nextElementPattern = document.getElementById("pattern" + counter);
-//     nextElementPattern.classList.remove('carouselInactive');
-//   }
-// }
-
-// const backward = () => {
-//   const elementPattern = document.getElementById("pattern" + counter);
-//   if(!elementPattern.classList.contains('carouselInactive')) {
-//     elementPattern.classList.add('carouselInactive');
-//     counter = counter - 1;
-//     if(counter < 1){
-//       counter = 3;
-//     }
-//     console.log(counter);
-//     const nextElementPattern = document.getElementById("pattern" + counter);
-//     nextElementPattern.classList.remove('carouselInactive');
-//   }
-// }
-
-// function changeValuePaused() {
-//   paused = !paused;
-// }
-
-// forwardButton.addEventListener('click', forward);
-// backwardButton.addEventListener('click', backward);
-// pauseButton.addEventListener('click', changeValuePaused);
-
-// setInterval(() => {
-//   if (!paused) {
-//     console.log('deu certo');
-//     forward();
-//   }
-// }, 3000);
-
-
-
-
-
-// -- IMPORTANT: -- First Functions, the backward function is not working:
-// function changePattern() {
-//   const elementPattern = document.getElementById("pattern" + counter);
-//   if(!elementPattern.classList.contains('carouselInactive')) {
-//     elementPattern.classList.add('carouselInactive');
-//     // console.log(elementPattern);
-//     forward();
-//     console.log(counter);
-//     const nextElementPattern = document.getElementById("pattern" + counter);
-//     // console.log(nextElementPattern);
-//     nextElementPattern.classList.remove('carouselInactive');
-//   }
-// }
-
-// const forward = () => {
-//   counter = counter + 1;
-//   if(counter > 3){
-//     counter = 1;
-//   }
-// }
-
-// const backward = () => {
-//   counter = counter - 1;
-//   if(counter < 1){
-//     counter = 3;
-//   }
-// }
-
-// function changeValuePaused() {
-//   paused = !paused;
-// }
-
-// forwardButton.addEventListener('click', forward);
-// backwardButton.addEventListener('click', backward);
-// pauseButton.addEventListener('click', changeValuePaused);
-
-// setInterval(() => {
-//   if (!paused) {
-//     console.log('deu certo');
-//     changePattern();
-//   }
-// }, 5000);
